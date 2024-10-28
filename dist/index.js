f58186ce7095249ebc9cc774628edb78eba0f76a
kingjeongkong <wjdqls9223@gmail.com>
2024-10-28 17:33:39 +0900

    Union, Enum

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,34 @@
 "use strict";
 let id = 5;
-console.log('🚀 ~ id:', id);
+let company = 'King cor.';
+let isPublished = true;
+let x = 'hello';
+let age;
+age = 30;
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'hello'];
+let person = [1, 'one', true];
+let employee;
+employee = [
+    [1, 'Brad'],
+    [2, 'John'],
+    [3, 'Jake']
+];
+let pid = 22;
+pid = '22';
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 3] = "Up";
+    Direction1[Direction1["Down"] = 4] = "Down";
+    Direction1[Direction1["Left"] = 5] = "Left";
+    Direction1[Direction1["Right"] = 6] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+console.log(Direction2.Up);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -25,3 +25,23 @@ employee = [
   [2, 'John'],
   [3, 'Jake']
 ];
+
+// Union, 타입 선언 시 두 가지의 타입을 가질 수 있게 함.
+let pid: number | string = 22;
+pid = '22';
+
+// Enum
+enum Direction1 {
+  Up = 3,
+  Down,
+  Left,
+  Right
+}
+
+enum Direction2 {
+  Up = 'Up',
+  Down = 'Down',
+  Left = 'Left',
+  Right = 'Right'
+}
+console.log(Direction2.Up);
